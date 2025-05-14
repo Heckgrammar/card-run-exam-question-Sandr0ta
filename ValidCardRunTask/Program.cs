0de838d7c9c3e17e432f7bba8a6bc61df92e2423
Sandr0ta <144434115+Sandr0ta@users.noreply.github.com>
2025-05-14 22:21:33 +0100

    Update Program.cs

--- ValidCardRunTask/Program.cs
+++ ValidCardRunTask/Program.cs
@@ -19,6 +19,20 @@
             bool gameWon = false;
             // add your code here
 
+            int[] cards = new int[100];
+
+            bool gameWon = false;
+
+            for (int i = 0; i <= 95; i++)
+            {
+            
+                if (cards[i + 1] == cards[i] + 1 &&
+                    cards[i + 2] == cards[i] + 2 &&
+                    cards[i + 3] == cards[i] + 3 &&
+                    cards[i + 4] == cards[i] + 4)
+                {
+                    gameWon = true;
+                    break;
 
             // end of your code
             Console.WriteLine(gameWon);
